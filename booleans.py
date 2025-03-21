35d37c77f7cceb558e806774d47324857f4ec73c
Fabian Romano <fabian.ed.romano@gmail.com>
2025-03-21 16:17:54 -0300

    first commit

---
+++ booleans.py
@@ -0,0 +1,19 @@
+### Tipo de dato que toma dos valores Verdadero o Falso (True o False)
+
+a = True
+b = False
+
+### Nos ayudarán a tomar decisiones
+
+### Estructuras que devuelven verdadero (las que tienen contenido)
+string = bool("Hola mundo estoy aprendiendo Python")
+num = bool(2022)
+lista = bool(['Naranja', 'Manzana'])
+dict = bool({"nombre": "Fabian Romano"})
+
+### Estructuras que devuelven falso (todas están vacías de contenido)
+string2 = bool("")
+num2 = bool(0)
+lista2 = bool([])
+dict2 = bool({})
+none = bool(None)
